baee1f6e06a2b80f22612ff74005a5763b0b2e16
AnderP-cod <nemo001leyn@gmail.com>
2022-05-16 14:07:39 +0200

    Update 27.2.py

--- 27.2.py
+++ 27.2.py
@@ -1,11 +1,16 @@
 import random
 
-
 def secure_password_generator():
     for i in range(1000):
+        nub = random.randint(100,999)
+        great_letter = random.choice("abcdefghijklmnopqrstuvwxyz")
+        small_letter = random.choice("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+        item = random.choice(f"{great_letter}{small_letter}{nub}")
+        item2 = random.choice(f"{great_letter}{small_letter}{nub}")
+        item3 = random.choice(f"{great_letter}{small_letter}{nub}")
         passwor = open("passwords.txt","a")
         passwords = {
-            "passwords": random.randint(0,100000000)
+            "passwords": f"{item}{item2}{item3}{item3}{item2}{item}{item2}{item}"
         }
         passwor.write(str(passwords))
         yield passwords
